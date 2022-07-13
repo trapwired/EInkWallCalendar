5b7e57d59d455b990d4186044a68fef2036d6828
Dominic Weibel <dominic.weibel@inf.ethz.ch>
2022-07-13 13:39:36 +0200

    making it work via git

--- DisplayDrawer.py
+++ DisplayDrawer.py
@@ -71,13 +71,13 @@ def start_drawing():
         time.sleep(2)
 
         logging.info("3.read bmp file")
-        Himage = Image.open(os.path.join(picdir, '7in5_V2.bmp'))
+        Himage = Image.open(os.path.join('res', '7in5_V2.bmp'))
         epd.display(epd.getbuffer(Himage))
         time.sleep(2)
 
         logging.info("4.read bmp file on window")
         Himage2 = Image.new('1', (epd.width, epd.height), 255)  # 255: clear the frame
-        bmp = Image.open(os.path.join(picdir, '100x100.bmp'))
+        bmp = Image.open(os.path.join('res', '100x100.bmp'))
         Himage2.paste(bmp, (50, 10))
         epd.display(epd.getbuffer(Himage2))
         time.sleep(2)
