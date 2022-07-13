26780a751fff576d6ed0842b03bd12de10b6ea79
Dominic Weibel <dominic.weibel@inf.ethz.ch>
2022-07-13 13:55:21 +0200

    some improvements

--- DisplayDrawer.py
+++ DisplayDrawer.py
@@ -41,7 +41,7 @@ def start_drawing():
         # draw calendar
         draw.line((0, 50, epd.width, 50), fill=0)
         for i in range(1, 6):
-            draw.text(((i - 1) * 160 + 20, 0), days[i-1], font=font24, fill=0)
+            draw.text(((i - 1) * 160, 0), days[i-1], font=font24, fill=0)
             draw.line((i * 160, 0, i * 160, epd.height))
         # draw.line((0, 100, epd.width, 100), fill=1)
         # draw.line((0, 150, epd.width, 150), fill=2)
