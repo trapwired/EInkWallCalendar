17d8d724c264bf86a2357d801f520939c31a7e8a
Dominic Weibel <dominic.weibel@inf.ethz.ch>
2022-07-13 09:15:29 +0200

    get all events

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # EInkWallCalendar
 Using a e-ink display from Waveshare (7.5 inch, 2 colors) and a pi zero w, I create a wall mounted calendar that displays the current week from an ics file.
+
+# secrets.ini
+In the root directory, there should be a file called secrets.ini, with the following content:
+```
+[Calendar]
+link = https://some.link.to.a.calendar.ics.file
+```
--- main.py
+++ main.py
@@ -1,4 +1,49 @@
+import configparser
+import requests
+import datetime
+import recurring_ical_events
+from pytz import timezone
+
+from icalendar import Calendar
+
+
+def time_in_range(start, end, x):
+    """Return true if x is in the range [start, end]"""
+    if start <= end:
+        return start <= x <= end
+    else:
+        return start <= x or x <= end
 
 
 if __name__ == "__main__":
-    print("hi")
+    secrets = configparser.RawConfigParser()
+    secrets.read('secrets.ini', encoding="utf8")
+    link = secrets["Calendar"]["link2"]
+    myfile = requests.get(link)
+
+    #print(myfile.content)
+    now = datetime.date.today()
+    print(now)
+    in7days = now + datetime.timedelta(days=7)
+    gcal = Calendar.from_ical(myfile.content)
+    #localtz = timezone(gcal.get('X-WR-TIMEZONE'))
+    for component in gcal.walk():
+        if component.name == "VTIMEZONE":
+            localtz = timezone(component.get('TZID'))
+    # recurring events
+    events = recurring_ical_events.of(gcal).between(now, in7days)
+    for event in events:
+        summary = event['SUMMARY']
+        startTime = event['DTSTART'].dt
+        endTime = event['DTEND'].dt # TODO do the times match?
+        print(summary, startTime, endTime)
+    #print("HERE")
+    # for component in gcal.walk():
+    #     if component.name == "VEVENT":
+    #         temp = component.get('dtstart').dt
+    #         dateStart = temp if type(temp) == datetime.date else temp.date()
+    #         if time_in_range(now, in7days, dateStart):
+    #             print(component.get('summary'))
+    #             print(dateStart)
+    #             print(component.decoded('dtend'))
+    myfile.close()
